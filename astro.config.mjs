a06b46959dc220700777e2c09264e864b338f9ca
catboybinary <catboybinary@disroot.org>
2024-11-24 01:04:12 +0300

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), db()]
+});
